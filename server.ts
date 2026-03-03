d824c750059c1abd0410130a32923b50c806cf24
GankhulugD <gankhulug12345@gmail.com>
2026-03-03 13:28:36 +0800

    2

--- server.ts
+++ server.ts
@@ -1,25 +1,60 @@
 import express from "express";
 import type { Request, Response } from "express";
 
-const app = express();
-const port = 3000;
+const books = [
+  {
+    id: 2,
+    title: "Harry potter 1",
+    author: "bat",
+  },
+  {
+    id: 1,
+    title: "Harry potter 2",
+    author: "bat",
+  },
+];
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello!");
+const server = express();
+const port = "3000";
+
+server.use(express.json()); // body parser
+
+server.get("/", (req: Request, res: Response) => {
+  res.send("hello");
 });
 
-app.post("/", (req: Request, res: Response) => {
-  res.send("Post");
+// READ ALL (Get all books)
+server.get("/books", (req: Request, res: Response) => {
+  res.status(200).send(books);
 });
 
-app.put("/", (req: Request, res: Response) => {
-  res.send("PUT");
+// READ ONE (Get book by id)
+server.get("/books/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const book = books.find((book) => String(book.id) === String(id));
+  res.status(200).send(book);
 });
 
-app.delete("/", (req: Request, res: Response) => {
-  res.send("DELETE");
+// CREATE (Add new book)
+server.post("/books", (req: Request, res: Response) => {
+  const { title, author } = req.body;
+  const newBookId = books.length + 1;
+  const newBook = { id: newBookId, title, author };
+  books.push(newBook);
+  res.send(books);
 });
 
-app.listen(port, () => {
+// DELETE (Delete book by id)
+server.get("/books/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const book = books.find((book) => String(book.id) === String(id));
+  if(){
+  books.splice(book);
+  res.status(200).send(books);}else{
+    
+  }
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
