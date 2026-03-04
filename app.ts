b71f11bff6fca7020257c175578c9b2b234711dc
GankhulugD <gankhulug12345@gmail.com>
2026-03-04 12:28:12 +0800

    2

---
+++ app.ts
@@ -0,0 +1,59 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+interface Task {
+  id: number;
+  task: string;
+  isComplete: boolean;
+}
+
+const tasks: Task[] = [];
+
+const server = express();
+const port = 4000;
+
+server.use(express.json()); // body parser
+
+//Get (Read)
+server.get("/tasks", (req: Request, res: Response) => {
+  res.send(tasks);
+});
+
+//Post (Creat)
+server.post("/tasks", (req: Request, res: Response) => {
+  const { task, isComplete } = req.body;
+  const maxId =
+    tasks.length > 0 ? Math.max(...tasks.map((task) => task.id)) : 0;
+  const newTask = { id: maxId + 1, task, isComplete };
+  tasks.push(newTask);
+  res.send(tasks);
+});
+
+//Put (Update)
+server.put("/tasks/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { task, isComplete } = req.body;
+  const findedTask = tasks.findIndex((task) => String(task.id) === String(id));
+  if (findedTask !== -1) {
+    tasks[findedTask] = { ...tasks[findedTask], task, isComplete };
+    res.send(tasks);
+  } else {
+    res.status(404).send("not found id");
+  }
+});
+
+//Delete
+server.delete("/tasks/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const findedTask = tasks.findIndex((task) => String(task.id) === String(id));
+  if (findedTask !== -1) {
+    tasks.splice(findedTask, 1);
+    res.send(tasks);
+  } else {
+    res.status(404).send("not found id");
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- server.ts
+++ server.ts
@@ -38,20 +38,35 @@ server.get("/books/:id", (req: Request, res: Response) => {
 // CREATE (Add new book)
 server.post("/books", (req: Request, res: Response) => {
   const { title, author } = req.body;
-  const newBookId = books.length + 1;
-  const newBook = { id: newBookId, title, author };
+  const maxId =
+    books.length > 0 ? Math.max(...books.map((book) => book.id)) : 0;
+  const newBook = { id: maxId + 1, title, author };
   books.push(newBook);
   res.send(books);
 });
 
-// DELETE (Delete book by id)
-server.get("/books/:id", (req: Request, res: Response) => {
+// UPTADE (Update book by id)
+server.put("/books/:id", (req: Request, res: Response) => {
   const { id } = req.params;
-  const book = books.find((book) => String(book.id) === String(id));
-  if(){
-  books.splice(book);
-  res.status(200).send(books);}else{
-    
+  const { title, author } = req.body;
+  const findedBook = books.findIndex((book) => String(book.id) === String(id));
+  if (findedBook !== -1) {
+    books[findedBook] = { ...books[findedBook], title, author };
+    res.send(books);
+  } else {
+    res.status(404).send("not found id");
+  }
+});
+
+// DELETE (Delete book by id)
+server.delete("/books/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const findedBook = books.findIndex((book) => String(book.id) === String(id));
+  if (findedBook !== -1) {
+    books.splice(findedBook, 1);
+    res.status(200).send(books);
+  } else {
+    res.status(404).send("not found id");
   }
 });
 
